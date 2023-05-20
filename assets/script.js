2f06a8de66cb1e8ddf6917eeed67cb972bf510ed
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-05-20 15:30:15 -0400

    added character arrays, added password alert prompts

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-//---- Let me start by saying this code is totally optional and if you want to go in a completely different direction I support it. Go with your gut and what works for you because it will help you figure out your thought process.Let us know if you have any questions and don't be afraid to start a dialogue with fellow students!  ----------------------- Delete this before you push your code to github LOL 
-
+var specialCharacters = ['@', '%', '+', '\\', '/', '`', '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '_', '-', '.'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 
@@ -12,12 +12,8 @@ function getPasswordOptions() {
 
   // Variable to store length of password from user input
   var length = parseInt(
-    prompt('How many characters would you like your password to contain?'),
-    10
+    prompt('How many characters would you like your password to contain?')
   );
-
-  // YOU WILL NEED MORE CODE IN HERE!~!!!!!!!!-----------------------------------
-
 
   // Conditional statement to check if password length is a number. Prompts end if this evaluates false
   if (Number.isNaN(length)) {
@@ -25,17 +21,41 @@ function getPasswordOptions() {
     return null;
   }
 
+  if (length < 8) {
+    alert ("Minimum password length required: 8 characters");
+    return null;
+  }
+
+  if (length > 128) {
+    alert ("Maximum password length allowed: 128 characters");
+    return null;
+  }
+
   // Variable to store boolean regarding the inclusion of special characters
-  var hasSpecialCharacters = confirm(
-    'Click OK to confirm including special characters.'
-  );
+  var hasSpecialCharacters = confirm('Click OK to confirm including special characters.');
+
+  var hasNumericCharacters = confirm('Click OK to confirm including numeric characters');
+
+  var hasLowerCasedCharacters = confirm('Click OK to confirm including lowercase characters');
+
+  var hasUpperCasedCharacters = confirm('Click OK to confirm including uppercase characters');
+
+
+  if (hasSpecialCharacters === false && hasNumericCharacters === false && hasLowerCasedCharacters === false && hasUpperCasedCharacters === false) {
+    alert("Must select at least one character type");
+    return null;
+  }
+
 
    // Object to store user input
    var passwordOptions = {
     length: length,
-    // add more properties and values here
-   }
-
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters,
+   };
+   // IF ALL PARAMETERS ABOVE ARE MET, THIS CALLED FUNCTION WILL RETURN passwordOptions object
    return passwordOptions;
 }
 
